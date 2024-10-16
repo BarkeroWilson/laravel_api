9e4a56677a06de314b452512bf3913b19f9576c4
WILSONBARKERO\barke <barkero.wilson@gmail.com>
2024-10-16 17:15:56 +0800

    CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
 
 Route::prefix('V1')->group(function () {
     route::apiResource('/tasks',TaskController::class);
+    route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
 Route::get('/user', function (Request $request) {
